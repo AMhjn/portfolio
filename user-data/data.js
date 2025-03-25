f927dba772202ac515692f97e3b55d9b819ae2a2
Abhir Mahajan <abhirmhjn@gmail.com>
2025-03-25 04:11:33 -0400

    New Layout

---
+++ user-data/data.js
@@ -0,0 +1,130 @@
+export const bio = [
+  "A <strong>Full Stack Software Engineer</strong> with experience in building scalable, high-performance applications that solve real-world problems.",
+  "An <strong>innovation-driven developer</strong> constantly learning and adapting to create impactful software products.",
+  "A <strong>Collaborative team player</strong> who thrives in fast-paced environments and enjoys tackling complex challenges.",
+  "<strong>Actively seeking full-time Software Engineering roles for 2025.</strong>",
+];
+
+export const skills = [
+  "Java, Python, C, C++, C#, Kotlin, JavaScript, TypeScript, HTML, CSS.",
+  "Spring Boot, Junit, Node.js, Express.js, Angular, React.",
+  "AWS, REST APIs, Kafka, SaaS, Docker, Kubernetes, Microservices, System Design, CI/CD.",
+  "MySQL, SQL Server, MongoDB, PostgreSQL, Redis.",
+  "Git, Android Studio, Jira, Agile Development, Scrum, Jenkins",
+];
+
+export const experience = [
+  {
+    title: "Volkswagen Group Technology Solutions",
+    duration: "August 2021 - July 2023",
+    subtitle: "Software Engineer",
+    details: [
+      "Engineered and deployed the 'International Tax Documents' portal using Angular, Java, Spring Boot, and MySQL, <b>automating</b> document submission, approval, and delegation workflows, decreasing manual effort, and achieving <b>70%</b> client adoption.",
+      "Led a large-scale data migration from 'IRIS' MySQL database to 'IRIS 2.0' database, normalizing <b>100+ complex schemas</b>, ensuring <b>100% data integrity<b>, and accomplishing a seamless transition with less than 1% downtime and zero data loss.",
+      "Designed UI and <b>REST APIs</b> for 'IRIS' system, optimizing invoice creation, approval, and feedback workflows using Angular, Node.js, Express.js, and MongoDB, driving a <b>40%</b> increase in user adoption and reducing manual processing time.",
+      "<b>Mentored 3 interns</b>, improving onboarding efficiency and integration time by <b>50%<b>, while conducting detailed code reviews, ensuring best coding practices, and improving system reliability.",
+      "Implemented comprehensive <b>Logging Architecture<b> across Microservices, accelerating issue diagnosis time by <b>60%</b>, boosting system reliability by 40%, and enhancing customer satisfaction through faster issue resolution.",
+      "Earned prestigious <b>'Debutant'</b> and <b>'Instapat'</b> awards for problem-solving and innovative mindset that boosted customer engagement and increased IRIS project efficiency by <b>40%</b>, demonstrating ability to drive project success",
+    ],
+    tags: ["Java", "Spring Boot", "MySQL", "Node.js", " Express.js", "MongoDB", "Angular", "REST APIs", "Microservices"],
+    icon: "truck ",
+  },
+  {
+    title: "Paytm",
+    duration: "February 2021 - July 2021",
+    subtitle: "Software Engineer Intern",
+    details: [
+      "Enhanced Paytm’s mobile app stability by identifying and resolving critical performance issues, resulting in a <b>25% reduction in post-release defects</b> and a smoother user experience.",
+      "<b>Automated</b> and optimized testing processes using Java, Selenium, and Appium, executing <b>1,000+</b> test cases across Android and iOS in an Agile environment, diminishing bug detection time by <b>15%</b> and strengthening overall app quality.",
+    ],
+    tags: [
+      "Java",
+      "Selenium",
+      "Appium",
+      "Manual Testing",
+      "Automation Testing",
+      "Mobile App Testing",
+    ],
+    icon: "truck",
+  }
+  
+];
+
+export const blogItems = [
+  {
+    link: "https://github.com/AMhjn/Music_Streaming_Backend",
+    title: "Melodify - Music Streaming Application",
+    content: "Built a scalable Music Streaming Application that enables seamless playback, offline downloads, and user-uploaded songs.",
+    categories: ["Android", "Kotlin", "Java", "Spring Boot", "MySQL", "AWS S3", "Kafka", "Redis"],
+  },
+  {
+    link: "https://github.com/AMhjn/ECommerce",
+    title: "E-Commerce Platform",
+    content: "Designed and developed an eCommerce web application enabling seamless product browsing, secure transactions, and order management.",
+    categories: ["Java", "Spring Boot", "MySQL", "Kafka", "Redis"],
+  },
+  {
+    link: "https://github.com/vyyellap/OOD_CoursePlus",
+    title: "Online Course Streaming Platform",
+    content: "Developed an online course streaming platform that provides seamless access to educational content with real-time progress tracking and personalized recommendations.",
+    categories: ["Node.js", "Express.js", "MySQL", "Angular", "Redis"],
+  }
+  
+];
+
+export const trekking = [];
+
+export const education = [
+    {
+      title: "Master of Science in Computer & Information Science",
+      duration: "August 2023 - May 2025",
+      subtitle: "Syracuse University, New York",
+      details: [],
+      tags: ["Advanced Data Structures & Algorithms",
+      "Object Oriented Design",
+      "Database Management System",
+      "Artificial Intelligence",
+      "Computer Architecture",
+      "Android Programming",],
+      icon: "graduation-cap",
+    },
+    {
+    title: "B.Tech. in Computer Science and Engineering",
+    duration: "August 2017 - July 2021",
+    subtitle: "Dr. A.P.J. Abdul Kalam Technical University, India",
+    details: [],
+    tags: [
+      "Data Structures & Algorithms",
+      "Operating Systems",
+      "Database Management System",
+      "Object Oriented Programming",
+      "Computer Networks",
+      "Software Engineering",],
+    icon: "book",
+  },
+  
+];
+
+export const footer = [
+  {
+    label: "Links",
+    data: [
+        {
+          text: "Linkedin",
+          link: "https://www.linkedin.com/in/abhirmahajan003/",
+        },
+      {
+        text: "Github",
+        link: "https://github.com/Amhjn",
+      },
+      {
+        text: "Leetcode",
+        link: "https://leetcode.com/u/AbhirMhjn/",
+      },
+    ],
+  },
+  {
+    label: "copyright-text",
+    data: ["Made with &hearts;"],
+  },
+];
